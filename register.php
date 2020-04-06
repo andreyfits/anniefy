52096f0d38265c0e73267a48bbd0e85b164619d8
Andrey Fits <andreyfits@gmail.com>
2020-04-06 19:57:24 +0300

    Separating our code

--- register.php
+++ register.php
@@ -1,99 +1,58 @@
 <?php
-
-function sanitizeFormPassword($inputText)
-{
-	$inputText = strip_tags($inputText);
-	return $inputText;
-}
-
-function sanitizeFormUsername($inputText)
-{
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	return $inputText;
-}
-
-function sanitizeFormString($inputText)
-{
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	$inputText = ucfirst(strtolower($inputText));
-	return $inputText;
-}
-
-if (isset($_POST['loginButton'])) {
-	//Login button is pressed
-}
-
-if (isset($_POST['registerButton'])) {
-	//Register button is pressed
-	$username = sanitizeFormUsername($_POST['username']);
-
-	$firstName = sanitizeFormString($_POST['firstName']);
-
-	$lastName = sanitizeFormString($_POST['lastName']);
-
-	$email = sanitizeFormString($_POST['email']);
-
-	$email2 = sanitizeFormString($_POST['email2']);
-
-	$password = sanitizeFormPassword($_POST['password']);
-
-	$password2 = sanitizeFormPassword($_POST['password2']);
-}
-
+include "includes/handlers/register-handler.php";
+include "includes/handlers/login-handler.php";
 ?>
 
 <html>
 <head>
-    <title>Welcome to Anniefy!</title>
+    <title> Welcome to Anniefy!</title>
 </head>
 <body>
 <div id="inputContainer">
     <form id="loginForm" action="register.php" method="post">
-        <h2>Login to your account</h2>
+        <h2> Login to your account </h2>
         <p>
-            <label for="loginUsername">Username</label>
+            <label for="loginUsername"> Username</label>
             <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. annaGnatyuck" required>
         </p>
         <p>
-            <label for="loginPassword">Password</label>
+            <label for="loginPassword"> Password</label>
             <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
         </p>
-        <button type="submit" name="loginButton">LOG IN</button>
+        <button type="submit" name="loginButton"> LOG IN</button>
     </form>
 
     <form id="registerForm" action="register.php" method="post">
-        <h2>Create your free account</h2>
+        <h2> Create your free account </h2>
         <p>
-            <label for="username">Username</label>
+            <label for="username"> Username</label>
             <input id="username" name="username" type="text" placeholder="e.g. annaGnatyuck" required>
         </p>
         <p>
-            <label for="firstName">First Name</label>
+            <label for="firstName"> First Name </label>
             <input id="firstName" name="firstName" type="text" placeholder="e.g. Anna" required>
         </p>
         <p>
-            <label for="lastName">Last Name</label>
+            <label for="lastName"> Last Name </label>
             <input id="lastName" name="lastName" type="text" placeholder="e.g. Gnatyuck" required>
         </p>
         <p>
-            <label for="email">Email</label>
+            <label for="email"> Email</label>
             <input id="email" name="email" type="email" placeholder="e.g. anna@gmail.com" required>
         </p>
         <p>
-            <label for="email2">Confirm email</label>
+            <label for="email2"> Confirm email </label>
             <input id="email2" name="email2" type="email" placeholder="e.g. anna@gmail.com" required>
         </p>
         <p>
-            <label for="password">Password</label>
+            <label for="password"> Password</label>
             <input id="password" name="password" type="password" placeholder="Your password" required>
         </p>
         <p>
-            <label for="password2">Confirm password</label>
+            <label for="password2"> Confirm password </label>
             <input id="password2" name="password2" type="password" placeholder="Your password" required>
         </p>
-        <button type="submit" name="registerButton">SIGN UP</button>
+        <button type="submit" name="registerButton"> SIGN UP</button>
     </form>
 </div>
 </body>
