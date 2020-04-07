6d0de7bd9ffff2f5f2d7ebdfbd4e8270faa8c4e5
Andrey Fits <andreyfits@gmail.com>
2020-04-07 18:14:18 +0300

    Connecting to our database from PHP

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+ob_start();
+
+$timezone = date_default_timezone_set("Europe/Moscow");
+
+$con = mysqli_connect("localhost", "root", "", "anniefy");
+
+if (mysqli_connect_errno()) {
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
