b58766caef25cb21cadd266bfabebe972b72a117
Andrey Fits <andreyfits@gmail.com>
2020-04-07 15:59:21 +0300

    Outputting error messages

--- register.php
+++ register.php
@@ -16,47 +16,55 @@ include "includes/handlers/login-handler.php";
     <form id="loginForm" action="register.php" method="post">
         <h2> Login to your account </h2>
         <p>
-            <label for="loginUsername"> Username</label>
+            <label for="loginUsername">Username</label>
             <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. annaGnatyuck" required>
         </p>
         <p>
-            <label for="loginPassword"> Password</label>
+            <label for="loginPassword">Password</label>
             <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
         </p>
-        <button type="submit" name="loginButton"> LOG IN</button>
+        <button type="submit" name="loginButton">LOG IN</button>
     </form>
 
     <form id="registerForm" action="register.php" method="post">
-        <h2> Create your free account </h2>
+        <h2>Create your free account</h2>
         <p>
-            <label for="username"> Username</label>
+			<?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
+            <label for="username">Username</label>
             <input id="username" name="username" type="text" placeholder="e.g. annaGnatyuck" required>
         </p>
         <p>
-            <label for="firstName"> First Name </label>
+			<?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
+            <label for="firstName">First Name</label>
             <input id="firstName" name="firstName" type="text" placeholder="e.g. Anna" required>
         </p>
         <p>
-            <label for="lastName"> Last Name </label>
+			<?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
+            <label for="lastName">Last Name</label>
             <input id="lastName" name="lastName" type="text" placeholder="e.g. Gnatyuck" required>
         </p>
         <p>
-            <label for="email"> Email</label>
+			<?php echo $account->getError("Your emails don't match"); ?>
+			<?php echo $account->getError("Email is invalid"); ?>
+            <label for="email">Email</label>
             <input id="email" name="email" type="email" placeholder="e.g. anna@gmail.com" required>
         </p>
         <p>
-            <label for="email2"> Confirm email </label>
+            <label for="email2">Confirm email</label>
             <input id="email2" name="email2" type="email" placeholder="e.g. anna@gmail.com" required>
         </p>
         <p>
-            <label for="password"> Password</label>
+			<?php echo $account->getError("Your passwords don't match"); ?>
+			<?php echo $account->getError("Your password can only contain numbers and letters"); ?>
+			<?php echo $account->getError("Your password must be between 5 and 30 characters"); ?>
+            <label for="password">Password</label>
             <input id="password" name="password" type="password" placeholder="Your password" required>
         </p>
         <p>
-            <label for="password2"> Confirm password </label>
+            <label for="password2">Confirm password</label>
             <input id="password2" name="password2" type="password" placeholder="Your password" required>
         </p>
-        <button type="submit" name="registerButton"> SIGN UP</button>
+        <button type="submit" name="registerButton">SIGN UP</button>
     </form>
 </div>
 </body>
