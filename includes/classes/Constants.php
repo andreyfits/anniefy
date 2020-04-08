fc00871184e6eabbba62fb4b74bb4367485d065d
Andrey Fits <andreyfits@gmail.com>
2020-04-08 14:31:43 +0300

    Last remaining validation checks

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -8,7 +8,9 @@ class Constants
 	public static string $passwordCharacters = "Your password must be between 5 and 30 characters";
 	public static string $emailInvalid = "Email is invalid";
 	public static string $emailsDoNotMatch = "Your emails don't match";
+	public static string $emailTaken = "This email is already in use";
 	public static string $lastNameCharacters = "Your last name must be between 2 and 25 characters";
 	public static string $firstNameCharacters = "Your first name must be between 2 and 25 characters";
 	public static string $usernameCharacters = "Your username must be between 5 and 25 characters";
+	public static string $usernameTaken = "This username already exists";
 }
--- register.php
+++ register.php
@@ -40,6 +40,7 @@ function getInputValue($name)
         <h2>Create your free account</h2>
         <p>
 			<?php echo $account->getError(Constants::$usernameCharacters); ?>
+			<?php echo $account->getError(Constants::$usernameTaken); ?>
             <label for="username">Username</label>
             <input id="username" name="username" type="text" placeholder="e.g. annaGnatyuck" value="<?php
 			getInputValue('username'); ?>" required>
@@ -59,6 +60,7 @@ function getInputValue($name)
         <p>
 			<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
 			<?php echo $account->getError(Constants::$emailInvalid); ?>
+			<?php echo $account->getError(Constants::$emailTaken); ?>
             <label for="email">Email</label>
             <input id="email" name="email" type="email" placeholder="e.g. anna@gmail.com"
                    value="<?php getInputValue('email'); ?>" required>
