d0a0f5e614dbed56ea8e66292e501bbf5629ca96
Andrey Fits <andreyfits@gmail.com>
2020-04-08 15:12:33 +0300

    Sessions

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
+<?php
+require_once "includes/config.php";
+
+if (isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+	header("Location: register.php");
+}
+
+?>
 <!DOCTYPE html>
 <head>
-	<title>Welcome to Anniefy!</title>
+    <title>Welcome to Anniefy!</title>
 </head>
 <body>
 <?php echo "Hi, Anniefy!"; ?>
