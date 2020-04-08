a21abf22ec7565e9d43b22dbcc943867ec381408
Andrey Fits <andreyfits@gmail.com>
2020-04-08 18:45:25 +0300

    Showing and hiding the forms on press

--- register.php
+++ register.php
@@ -21,6 +21,8 @@ function getInputValue($name)
 <head>
     <title> Welcome to Anniefy!</title>
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
 </head>
 <body>
 <div id="background">
