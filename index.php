321436283923a949742f8cf242f794579a37837c
Andrey Fits <andreyfits@gmail.com>
2020-04-09 17:09:39 +0300

    Progress bar parent elements

--- index.php
+++ index.php
@@ -39,6 +39,11 @@ if (isset($_SESSION['userLoggedIn'])) {
                         <img src="assets/images/icons/repeat.png" alt="Repeat">
                     </button>
                 </div>
+                <div class="playbackBar">
+                    <span class="progressTime current">0.00</span>
+                    <div class="progressBar"></div>
+                    <span class="progressTime remaining">0.00</span>
+                </div>
             </div>
         </div>
         <div id="nowPlayingRight"></div>
