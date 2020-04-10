af03762672639eb03312c167d6a899d043f89000
Andrey Fits <andreyfits@gmail.com>
2020-04-10 16:41:34 +0300

    Getting album id from url

---
+++ album.php
@@ -0,0 +1,17 @@
+<?php require_once "includes/header.php";
+
+	if (isset($_GET['id'])) {
+		$albumId = $_GET['id'];
+	} else {
+		header("Location: index.php");
+	}
+
+	$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+	$album = mysqli_fetch_array($albumQuery);
+
+	echo $album['title'];
+?>
+
+
+
+<?php require_once "includes/footer.php" ?>
