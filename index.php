6e36daf7d97bd2b3ceec667119a20bbb905d2e44
Andrey Fits <andreyfits@gmail.com>
2020-04-10 14:47:02 +0300

    Getting albums from the database

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php require_once "includes/header.php" ?>
+    <h1 class="pageHeadingBig">You Might Also like</h1>
+    <div class="gridViewContainer">
+		<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums");
+		while ($row = mysqli_fetch_array($albumQuery)) {
+			echo $row['title'] . "<br>";
+		}
+		?>
+    </div>
 <?php require_once "includes/footer.php" ?>
