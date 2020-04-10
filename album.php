e95bc18c121ac6418a5c58116590f0038e4778dd
Andrey Fits <andreyfits@gmail.com>
2020-04-10 16:47:19 +0300

    Getting the artist of the album

--- album.php
+++ album.php
@@ -1,17 +1,23 @@
 <?php require_once "includes/header.php";
 
-	if (isset($_GET['id'])) {
-		$albumId = $_GET['id'];
-	} else {
-		header("Location: index.php");
-	}
+if (isset($_GET['id'])) {
+	$albumId = $_GET['id'];
+} else {
+	header("Location: index.php");
+}
 
-	$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
-	$album = mysqli_fetch_array($albumQuery);
+$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+$album = mysqli_fetch_array($albumQuery);
 
-	echo $album['title'];
+$artistId = $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+echo $album['title'] . "<br>";
+echo $artist['name'];
+
 ?>
-
 
 
 <?php require_once "includes/footer.php" ?>
