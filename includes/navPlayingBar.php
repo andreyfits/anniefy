534004e7da78bf12f78707bea91f63280c8dd3eb
Andrey Fits <andreyfits@gmail.com>
2020-04-12 18:00:51 +0300

    Repeat button

--- includes/navPlayingBar.php
+++ includes/navPlayingBar.php
@@ -87,6 +87,12 @@ $jsonArray = json_encode($resultArray);
         setTrack(trackToPlay, currentPlaylist, true);
     }
 
+    function setRepeat() {
+        repeat = !repeat;
+        var imageName = repeat ? "repeat-active.png" : "repeat.png";
+        $(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
+    }
+
     function setTrack(trackId, newPlaylist, play) {
 
         currentIndex = currentPlaylist.indexOf(trackId);
@@ -172,7 +178,7 @@ $jsonArray = json_encode($resultArray);
                     <button class="controlButton next" title="Next button" onclick="nextSong()">
                         <img src="assets/images/icons/next.png" alt="Next">
                     </button>
-                    <button class="controlButton repeat" title="Repeat button">
+                    <button class="controlButton repeat" title="Repeat button" onclick="setRepeat()">
                         <img src="assets/images/icons/repeat.png" alt="Repeat">
                     </button>
                 </div>
