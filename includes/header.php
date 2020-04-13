38c5496d969a45b01ed0a2eb49c0dc3762493430
Andrey Fits <andreyfits@gmail.com>
2020-04-13 14:37:39 +0300

    UserLoggedIn JavaScript variable

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@ require_once "includes/classes/Song.php";
 
 if (isset($_SESSION['userLoggedIn'])) {
 	$userLoggedIn = $_SESSION['userLoggedIn'];
+    echo "<script>userLoggedIn = '$userLoggedIn';</script>";
 } else {
 	header("Location: register.php");
 }
