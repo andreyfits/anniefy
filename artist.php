12b40a06118992c93eae4abddd8230264240781c
Andrey Fits <andreyfits@gmail.com>
2020-04-13 19:28:48 +0300

    Styling the artist info section

--- artist.php
+++ artist.php
@@ -17,7 +17,7 @@ $artist = new Artist($con, $artistId);
         <div class="artistInfo">
             <h1 class="artistName"><?php echo $artist->getName(); ?></h1>
             <div class="headerButtons">
-                <button class="button">Play</button>
+                <button class="button green">PLAY</button>
             </div>
         </div>
     </div>
