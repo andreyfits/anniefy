75e26d442fd44c46da17578f0a904c3f6d58b6bc
Andrey Fits <andreyfits@gmail.com>
2020-04-14 15:25:53 +0300

    Searching when the user stops typing

--- search.php
+++ search.php
@@ -3,13 +3,30 @@ require_once "includes/includedFiles.php";
 
 if (isset($_GET['term'])) {
 	$term = urldecode($_GET['term']);
-}
-else {
+} else {
 	$term = "";
 }
 ?>
 
 <div class="searchContainer">
-	<h4>Search for an artist, album or song</h4>
-	<input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+    <h4>Search for an artist, album or song</h4>
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.value = this.value">
 </div>
+
+<script>
+
+    $(".searchInput").focus();
+
+    $(function () {
+        var timer;
+
+        $(".searchInput").keyup(function () {
+            clearTimeout(timer);
+
+            timer = setTimeout(function () {
+                var val = $(".searchInput").val();
+                openPage("search.php?term=" + val);
+            }, 2000);
+        })
+    });
+</script>
