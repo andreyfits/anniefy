807da694c7e46dd8e0ad780876e76faeb72e1c87
Andrey Fits <andreyfits@gmail.com>
2020-04-15 16:20:06 +0300

    Searching for albums

--- search.php
+++ search.php
@@ -32,6 +32,8 @@ if (isset($_GET['term'])) {
     });
 </script>
 
+<?php if ($term == "") exit(); ?>
+
 <div class="trackListContainer borderBottom">
     <h2>SONGS</h2>
     <ul class="trackList">
@@ -88,7 +90,6 @@ if (isset($_GET['term'])) {
 
 <div class="artistsContainer borderBottom">
     <h2>ARTISTS</h2>
-
 	<?php
 
 	$artistsQuery = mysqli_query($con, "SELECT id FROM artists WHERE name LIKE '$term%' LIMIT 10");
@@ -104,6 +105,28 @@ if (isset($_GET['term'])) {
 			      <div class = 'artistName'>
 				      <span role='link' tabindex='0' onclick='openPage(\"artist.php?id=" . $artistFound->getId() . "\")'>" . $artistFound->getName() . "</span>
                   </div>
+              </div>";
+	}
+
+	?>
+</div>
+
+<div class="gridViewContainer">
+    <h2>ALBUMS</h2>
+	<?php
+
+	$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE title LIKE '$term%' LIMIT 10");
+
+	if (mysqli_num_rows($albumQuery) == 0) {
+		echo "<span class='noResults'>No albums found matching " . $term . "</span>";
+	}
+
+	while ($row = mysqli_fetch_array($albumQuery)) {
+		echo "<div class='gridViewItem'>
+			      <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+				      <img src='" . $row['artworkPath'] . "' alt='Album'>
+				          <div class='gridViewInfo'>" . $row['title'] . "</div>
+				  </span>
               </div>";
 	}
 
