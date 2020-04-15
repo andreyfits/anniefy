3c1dd9eb4d2bff5cf6e04e3ccdf887a447a420ce
Andrey Fits <andreyfits@gmail.com>
2020-04-15 17:00:40 +0300

    Creating the playlist page

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php
+require_once "includes/includedFiles.php";
+?>
+
+<div class="playlistsContainer">
+    <div class="gridViewContainer">
+        <h2>PLAYLISTS</h2>
+        <div class="buttonItems">
+            <button class="button green">NEW PLAYLIST</button>
+        </div>
+    </div>
+</div>
