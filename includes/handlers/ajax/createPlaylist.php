be45368e32f734ba5bd1cc347935cdcac93122db
Andrey Fits <andreyfits@gmail.com>
2020-04-15 17:48:34 +0300

    Create playlist file

--- includes/handlers/ajax/createPlaylist.php
+++ includes/handlers/ajax/createPlaylist.php
@@ -1,1 +1,14 @@
 <?php
+require_once "../../config.php";
+
+if (isset($_POST['name']) && isset($_POST['username'])) {
+
+	$name = $_POST['name'];
+	$username = $_POST['username'];
+	$date = date("Y-m-d H:i:s");
+
+	$query = mysqli_query($con, "INSERT INTO playlists VALUES('', '$name', '$username', '$date')");
+}
+else {
+	echo "Name or username parameters not passed into file";
+}
