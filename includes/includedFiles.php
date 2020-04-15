fa29012f650ec585c5b9aa1e52c33e231441740e
Andrey Fits <andreyfits@gmail.com>
2020-04-15 17:59:23 +0300

    Creating the User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -2,9 +2,18 @@
 
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
 	require_once "includes/config.php";
+	require_once "includes/classes/User.php";
 	require_once "includes/classes/Artist.php";
 	require_once "includes/classes/Album.php";
 	require_once "includes/classes/Song.php";
+
+	if (isset($_GET['userLoggedIn'])) {
+		$userLoggedIn = new User($con, $_GET['userLoggedIn']);
+	} else {
+		echo "Username variable was not passed into page. Check the openPage JS function";
+		exit();
+	}
+
 } else {
 	require_once "includes/header.php";
 	require_once "includes/footer.php";
