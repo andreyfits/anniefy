70b97ab7ca6bdec653a67ab33a7d5c4e1c2b11f3
Andrey Fits <andreyfits@gmail.com>
2020-04-15 21:33:11 +0300

    Delete playlist function

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,26 @@ function createPlaylist() {
     var popup = prompt("Please enter the name of your playlist");
 
     if (popup != null) {
-        $.post("includes/handlers/ajax/createPlaylist.php", {name: popup, username: userLoggedIn}).done(function (error) {
+
+        $.post("includes/handlers/ajax/createPlaylist.php", {name: popup, username: userLoggedIn }).done(function (error) {
+
+            if (error !== "") {
+                alert(error);
+                return;
+            }
+
+            //do something when ajax returns
+            openPage("yourMusic.php");
+        });
+    }
+}
+
+function deletePlaylist(plalistId) {
+    var prompt = confirm("Are you sure you want to delete this playlist?");
+
+    if (prompt === true) {
+
+        $.post("includes/handlers/ajax/deletePlaylist.php", { playlistId: playlistId }).done(function (error) {
 
             if (error !== "") {
                 alert(error);
