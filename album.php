2c1fa31b5b34782466f5e5cada455781b870ddf2
Andrey Fits <andreyfits@gmail.com>
2020-04-15 22:00:03 +0300

    Creating the options menu

--- album.php
+++ album.php
@@ -59,3 +59,9 @@ $artistId = $artist->getId();
 
     </ul>
 </div>
+
+<nav class="optionsMenu">
+    <input type="hidden" class="songId">
+    <div class="item">Add to playlist</div>
+    <div class="item">Item 2</div>
+</nav>
